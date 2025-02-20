4085a3a36d4b11ac651878bfcecefeff55cfb8e7
shtrophic <christoph@liebender.dev>
2025-02-20 18:15:48 +0100

    use xs_dict_append correctly

--- activitypub.c
+++ activitypub.c
@@ -1026,9 +1026,9 @@ void notify(snac *snac, const char *type, const char *utype, const char *actor, 
                     from, email, subject);
 
         xs *mailinfo = xs_dict_new();
-        xs_dict_append(mailinfo, "from", from);
-        xs_dict_append(mailinfo, "to", email);
-        xs_dict_append(mailinfo, "body", xs_fmt("%s%s", header, body));
+        mailinfo = xs_dict_append(mailinfo, "from", from);
+        mailinfo = xs_dict_append(mailinfo, "to", email);
+        mailinfo = xs_dict_append(mailinfo, "body", xs_fmt("%s%s", header, body));
 
         enqueue_email(mailinfo, 0);
     }
