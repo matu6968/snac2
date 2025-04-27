73b807b8628cb3888deac790e6d81d3f53ba758a
shtrophic <christoph@liebender.dev>
2025-04-27 08:29:11 +0200

    remove unused variable

--- sandbox.c
+++ sandbox.c
@@ -61,8 +61,7 @@ LL_BEGIN(sbox_enter_linux_, const char* basedir, const char *address, int smtp_p
              LANDLOCK_ACCESS_FS_REMOVE_DIR      |
              LANDLOCK_ACCESS_FS_REMOVE_FILE     |
              LANDLOCK_ACCESS_FS_REFER_COMPAT,
-        s  = LANDLOCK_ACCESS_FS_MAKE_SOCK,
-        x  = LANDLOCK_ACCESS_FS_EXECUTE;
+        s  = LANDLOCK_ACCESS_FS_MAKE_SOCK;
     char *resolved_path = NULL;
 
     LL_PATH(basedir,                rf|rd|w|c);
