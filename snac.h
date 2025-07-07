f27c9cb87a4168683d1aab3c5fe94b751e1b129a
grunfink <grunfink@comam.es>
2025-07-07 09:42:51 +0200

    Version 2.80 RELEASED.

--- snac.h
+++ snac.h
@@ -1,7 +1,7 @@
 /* snac - A simple, minimalistic ActivityPub instance */
 /* copyright (c) 2022 - 2025 grunfink et al. / MIT license */
 
-#define VERSION "2.80-dev"
+#define VERSION "2.80"
 
 #define USER_AGENT "snac/" VERSION
 
