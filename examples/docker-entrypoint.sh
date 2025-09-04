6b8524ea8b51d14db300dd65437d6b857db45397
daltux <1medinnz@duck.com>
2025-09-04 15:25:24 -0300

    Add tzdata to container image
    Install Alpine package `tzdata` to the container image.
    When running it, set timezone from environment variable TZ if not empty.

--- examples/docker-entrypoint.sh
+++ examples/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
-if [ ! -e /data/data/server.json ]
-then
+if [ -n "$TZ" ] ; then
+	ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime \
+		&& echo "$TZ" > /etc/timezone
+fi
+if [ ! -e /data/data/server.json ] ; then
     echo -ne "0.0.0.0\r\n8001\r\nlocalhost\r\n\r\n\r\n" | snac init /data/data
     snac adduser /data/data testuser
 fi
 SSLKEYLOGFILE=/data/key snac httpd /data/data
+
